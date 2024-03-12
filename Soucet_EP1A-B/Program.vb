ae5c3556cb9d7653490367be5f79b8ac37e80516
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-03-12 13:42:23 +0100

    po hodině

--- Soucet_EP1A-B/Program.vb
+++ Soucet_EP1A-B/Program.vb
@@ -2,18 +2,20 @@
 
 Module Program
     Sub Main(args As String())
-
-        Okno(10, 20)
-
+        Okno(25, 5, "  Součet n čísel  ")
+        Okno(25, 8, " Zadej číslo:     ")
+        Okno(25, 11, " Součet je        ")
         Console.ReadKey()
     End Sub
 
-    Sub Okno(left As Integer, top As Integer)
+    Sub Okno(left As Integer, top As Integer, text As String)
+        Dim cara As String = New String("═", text.Length)
+
         Console.SetCursorPosition(left, top)
-        Console.WriteLine("╔═══╗")
+        Console.WriteLine($"╔═{cara}═╗")
         Console.SetCursorPosition(left, top + 1)
-        Console.WriteLine("║ X ║")
+        Console.WriteLine($"║ {text} ║")
         Console.SetCursorPosition(left, top + 2)
-        Console.WriteLine("╚═══╝")
+        Console.WriteLine($"╚═{cara}═╝")
     End Sub
 End Module
