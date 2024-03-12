c1e39cba25bc00b885bb25ed7a0a50a35044868d
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-03-12 13:02:44 +0100

    Add project files.

---
+++ Soucet_EP1A-B/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Okno(10, 20)
+
+        Console.ReadKey()
+    End Sub
+
+    Sub Okno(left As Integer, top As Integer)
+        Console.SetCursorPosition(left, top)
+        Console.WriteLine("╔═══╗")
+        Console.SetCursorPosition(left, top + 1)
+        Console.WriteLine("║ X ║")
+        Console.SetCursorPosition(left, top + 2)
+        Console.WriteLine("╚═══╝")
+    End Sub
+End Module
